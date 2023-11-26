49cfd99e250f971c513a695d56a83d02558df18d
joeyanovich <joedison.dias@gmail.com>
2023-11-26 08:48:25 -0300

    feat ✨: Summary component created

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const SummaryContainer = styled.section`
   width: 100%;
@@ -11,4 +11,32 @@ export const SummaryContainer = styled.section`
   gap: 2rem;
 
   margin-top: -5rem;
+`
+interface SummaryCardProps {
+  variant?: 'green'
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  padding: 2rem;
+  border-radius: 6px;
+
+  background: ${props => props.theme['gray-600']};
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    color: ${props => props.theme['gray-300']};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+
+  ${props => props.variant === 'green' && css`
+    background-color: ${props.theme['green-700']};
+  `}
 `
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function TransactionsPage() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
